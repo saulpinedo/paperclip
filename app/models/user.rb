4cb333270f3e686e901e9f74d114471fafc7f655
Sakretsu <sakretsu@WarMachine.(none)>
2015-03-17 00:12:59 -0400

    upload image done

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,11 @@
 class User < ActiveRecord::Base
-  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+  has_attached_file :avatar,
+                    :path => ":rails_root/public/system/:attachment/:id/:basename_:style.:extension",
+                    :url => "/system/:attachment/:id/:basename_:style.:extension",
+                    :styles => {
+                        :thumb    => ['100x100#',  :jpg, :quality => 70],
+                        :medium  => ['480x480#',  :jpg, :quality => 70]}
+
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
 end
